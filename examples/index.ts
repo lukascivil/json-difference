aa870a15e2e5b84857141911dfee985b14d42458
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2018-09-25 15:27:26 -0300

    update example

--- examples/index.ts
+++ examples/index.ts
@@ -3,9 +3,7 @@ import { JsonDiffer } from '../src/jsondiffer';
 const coffee = { color: { color1: 'black', color2: 'brown' }, special: true };
 const oil = { color: { color1: 'red', color2: 'blue' }, special2: false };
 
-const treewalkerjs = new JsonDiffer();
-let diff = treewalkerjs.getDiff(coffee, oil);
+const jsondifference = new JsonDiffer();
+let diff = jsondifference.getDiff(coffee, oil);
 
-console.log('coffee', coffee)
-console.log('oil', oil)
 console.log('diff', JSON.stringify(diff))

56dfe3cd3f23b1245599733acdac14e74f54498c
lukascivil <lukas_civil@hotmail.com>
2022-08-16 01:18:42 -0300

    fix: example code

--- examples/src/simple.ts
+++ examples/src/simple.ts
@@ -1,9 +1,8 @@
-import { JsonDiffer } from 'json-difference';
+import { getDiff } from 'json-difference';
 
 const coffee = { color: { color1: 'black', color2: 'brown' }, special: true };
 const oil = { color: { color1: 'red', color2: 'blue' }, special2: false };
 
-const jsondifference = new JsonDiffer();
-const diff = jsondifference.getDiff(coffee, oil);
+const diff = getDiff(coffee, oil);
 
 console.log('diff', JSON.stringify(diff));
--- examples/src/stress.ts
+++ examples/src/stress.ts
@@ -1,4 +1,4 @@
-import { JsonDiffer } from 'json-difference';
+import { getDiff } from 'json-difference';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -6,8 +6,9 @@ const oldJson = fs.readFileSync(path.join(__dirname, './assets/oldJson.json'), '
 const newJson = fs.readFileSync(path.join(__dirname, './assets/newJson.json'), 'utf8');
 
 const start: any = new Date();
-const jsondifference = new JsonDiffer();
-jsondifference.getDiff(oldJson, newJson);
+
+getDiff(oldJson, newJson);
+
 const end: any = new Date();
 const diffTime = end - start;
 

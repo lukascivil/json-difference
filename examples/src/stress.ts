a308b7544b345f201badba31bd0f01bb3bff3405
lukascivil <lukas_civil@hotmail.com>
2022-08-16 01:35:01 -0300

    fix: json object was expected

--- examples/src/stress.ts
+++ examples/src/stress.ts
@@ -2,8 +2,8 @@ import { getDiff } from 'json-difference';
 import * as fs from 'fs';
 import * as path from 'path';
 
-const oldJson = fs.readFileSync(path.join(__dirname, './assets/oldJson.json'), 'utf8');
-const newJson = fs.readFileSync(path.join(__dirname, './assets/newJson.json'), 'utf8');
+const oldJson = JSON.parse(fs.readFileSync(path.join(__dirname, './assets/oldJson.json'), 'utf8'));
+const newJson = JSON.parse(fs.readFileSync(path.join(__dirname, './assets/newJson.json'), 'utf8'));
 
 const start: any = new Date();
 

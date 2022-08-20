3e8530e3bc462f5784a010c4aca7ddff1fc15133
lukascivil <lukas_civil@hotmail.com>
2022-08-20 20:06:14 -0300

    feat: add lodash like result

--- src/core/get-struct-paths.ts
+++ src/core/get-struct-paths.ts
@@ -1,19 +1,26 @@
 // Models
 import { StructPaths } from '../models/jsondiffer.model'
 
-export const getStructPaths = (struct: any, paths: { [key: string]: any } = {}, currentPath = ''): StructPaths => {
+const generatePath = (isArray: boolean, currentPath: string, newPath: string, lodashLike: boolean): string => {
+  const prefix = lodashLike ? (isArray ? '[' : '.') : '/'
+  const suffix = lodashLike ? (isArray ? ']' : '') : isArray ? '[]' : ''
+  const path = currentPath !== '' ? `${currentPath}${prefix}${newPath}${suffix}` : `${newPath}${suffix}`
+
+  return path
+}
+
+export const getStructPaths = (struct: any, lodashLike = false, paths: { [key: string]: any } = {}, currentPath = ''): StructPaths => {
   for (const key of Object.keys(struct)) {
-    const path = currentPath !== '' ? `${currentPath}/${key}` : key
-    const finalPath = Array.isArray(struct) ? `${path}[]` : path
+    const path = generatePath(Array.isArray(struct), currentPath, key, lodashLike)
 
     if (typeof struct[key] === 'object') {
       if (Object.keys(struct[key]).length === 0) {
         paths[path] = struct[key]
       }
 
-      getStructPaths(struct[key], paths, finalPath)
+      getStructPaths(struct[key], lodashLike, paths, path)
     } else {
-      paths[finalPath] = struct[key]
+      paths[path] = struct[key]
     }
   }
 

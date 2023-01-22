60649f1b5d0d6a33aad143dfb1c6a165ede8ae3b
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2023-01-22 17:41:15 -0300

    chore: change types export

--- examples/src/stress.ts
+++ examples/src/stress.ts
@@ -1,15 +1,15 @@
-import { getDiff } from 'json-difference';
-import * as fs from 'fs';
-import * as path from 'path';
+import { getDiff } from 'json-difference'
+import * as fs from 'fs'
+import * as path from 'path'
 
-const oldJson = JSON.parse(fs.readFileSync(path.join(__dirname, './assets/oldJson.json'), 'utf8'));
-const newJson = JSON.parse(fs.readFileSync(path.join(__dirname, './assets/newJson.json'), 'utf8'));
+const oldJson = JSON.parse(fs.readFileSync(path.join(__dirname, './assets/oldJson.json'), 'utf8'))
+const newJson = JSON.parse(fs.readFileSync(path.join(__dirname, './assets/newJson.json'), 'utf8'))
 
-const start: any = new Date();
+const start: any = new Date()
 
-getDiff(oldJson, newJson);
+getDiff(oldJson, newJson)
 
-const end: any = new Date();
-const diffTime = end - start;
+const end: any = new Date()
+const diffTime = end - start
 
-console.info('Execution time: %dms', diffTime);
+console.info('Execution time: %dms', diffTime)

9dc182c7bcad56f415fe02ba3330b6735ebb1465
lucas cordeiro da Silva <lukascivil@users.noreply.github.com>
2023-01-22 18:17:54 -0300

    chore: adjust export (#36)

--- examples/src/stress.ts
+++ examples/src/stress.ts
@@ -1,13 +1,16 @@
+// Packages
 import { getDiff } from 'json-difference'
 import * as fs from 'fs'
 import * as path from 'path'
+import { Delta } from 'json-difference/dist/models/jsondiffer.model'
 
 const oldJson = JSON.parse(fs.readFileSync(path.join(__dirname, './assets/oldJson.json'), 'utf8'))
 const newJson = JSON.parse(fs.readFileSync(path.join(__dirname, './assets/newJson.json'), 'utf8'))
 
 const start: any = new Date()
 
-getDiff(oldJson, newJson)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const delta: Delta = getDiff(oldJson, newJson)
 
 const end: any = new Date()
 const diffTime = end - start

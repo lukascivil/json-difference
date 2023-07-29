61b11ce28105b2ed367bec8e4ae229e0a24f3d79
lucas cordeiro da Silva <lukascivil@users.noreply.github.com>
2023-07-29 10:07:37 -0300

    feat: add cli package (#52)
    * feat: add cli
    
    * Update package.json

---
+++ apps/json-difference-cli/bin/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs')
+const { getDiff } = require('json-difference')
+
+const options = yargs
+  .usage('Usage: -o <original> -m <modified>')
+  .option('o', { alias: 'original', describe: 'Json 1', type: 'string', demandOption: true })
+  .option('m', { alias: 'modified', describe: 'Json 2', type: 'string', demandOption: true }).argv
+
+const delta = getDiff(options.original, options.modified)
+
+console.log(delta)

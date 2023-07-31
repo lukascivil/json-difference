83cae8d116fdfc97afd4fc94889777abd75e3bb9
lucas cordeiro da Silva <lukascivil@users.noreply.github.com>
2023-07-31 01:24:00 -0300

    chore(monorepo): configure NX integrated repo and semantic release (#55)
    * chore: adjust project folders
    
    * chore: add nx build system
    
    * Update .gitignore
    
    * Update yarn.lock
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * chore: update yarn
    
    * Update .yarnrc.yml
    
    * Update .gitignore
    
    * chore: add ajusts to run the monorepo
    
    * update lock file
    
    * adjust monorepo
    
    * chore: add semantic release

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsConfigPaths()],
   base: '/json-difference',
   build: {
     outDir: '../../docs'
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-// Packages
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
-export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, 'src/index.ts'), name: 'json-difference' }, outDir: './dist.browser' },
-  plugins: [tsconfigPaths({ projects: ['tsconfig.browser.json'] })]
-})

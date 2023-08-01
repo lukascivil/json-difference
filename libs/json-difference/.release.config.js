5a897c79d4707e4a481e6d4d7b77f294f46b592e
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2023-08-01 02:19:49 -0300

    chore: add pending config

--- libs/json-difference/.release.config.js
+++ libs/json-difference/.release.config.js
@@ -21,7 +21,7 @@ module.exports = {
     [
       '@semantic-release/exec',
       {
-        prepareCmd: ` PACKAGE_NAME=${importPath} VERSION=\${nextRelease.version} npm run update-deps && VERSION=\${nextRelease.version} npm run bump-version:${libName}`
+        prepareCmd: ` PACKAGE_NAME=${importPath} VERSION=\${nextRelease.version} yarn run update-deps && VERSION=\${nextRelease.version} yarn run bump-version`
       }
     ],
     [

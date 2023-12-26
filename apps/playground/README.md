f2d801cf4fa2a7a7589f9bb402e9134c00c3028b
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2023-12-25 22:44:39 -0300

    chore: remove all

--- apps/playground/README.md
+++
@@ -1,3 +0,0 @@
-# GH Playground demo
-
-The playground is a live demo to help you better understand how the "json-difference" tool works.
--- libs/json-difference/src/core/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './get-diff'
-export * from './get-paths-diff'
-export * from './get-struct-paths'
-export * from './get-edited-paths'
